2ef3828737c614a9b546478b8aae27661853da39
Garrett Eastham <garrett.eastham@gmail.com>
2016-05-17 16:03:47 -0500

    trying to add breeze depencencies

--- src/main/scala/cs231n/nearestneighbor/NearestNeighbor.scala
+++ src/main/scala/cs231n/nearestneighbor/NearestNeighbor.scala
@@ -1,5 +1,18 @@
 package cs231n.nearestneighbor
 
+/*
+ *    Nearest Neight Classifier
+ *    -------------------------
+ *    Exposes two methods (train, predict) that take a training setup
+ *    of images and builds a nearest neighbor model and enables that model
+ *    to naively predict the class of a given image (from predict).
+ */
 class NearestNeighbor {
-  def hello = "Hello, world."
+  // Train
+  def train(train_images: Unit, train_labels: Unit) = sys.error("NearestNeighbor.train not implmented")
+
+  // Predict
+  def predict(model: Unit, test_images: Unit) = sys.error("NearestNeighbor.predict not implmented")
+
+  def hello = "Yep, still compiles and runs!"
 }
