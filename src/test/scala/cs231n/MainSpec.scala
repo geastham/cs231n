42bd5997f1df8bafe6e936d3dabadafa033c422b
Garrett Eastham <garrett.eastham@gmail.com>
2016-05-17 15:46:54 -0500

    more renaming

--- src/test/scala/cs231n/MainSpec.scala
+++ src/test/scala/cs231n/MainSpec.scala
@@ -4,9 +4,9 @@ import org.scalatest._
 
 class MainSpec extends FlatSpec {
 
-    "A Greeter" should "return a greeting" in {
+    /*"A Greeter" should "return a greeting" in {
       val greet = new Greetings
       assert(greet.greeting == "It works!")
-    }
+    }*/
 
 }
