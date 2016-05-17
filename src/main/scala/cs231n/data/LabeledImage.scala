4ef547e9e50d45ef2a84b7fbb0afb983e5cc574c
Garrett Eastham <garrett.eastham@gmail.com>
2016-05-17 18:00:23 -0500

    implementing labeled image and nerest neighbor class

--- src/main/scala/cs231n/data/LabeledImage.scala
+++ src/main/scala/cs231n/data/LabeledImage.scala
@@ -1,20 +1,28 @@
 package cs231n.data
 
+import breeze.linalg._
+
 /*
  *    Labeled Image
  *    -------------------------
- *    Support class for encapsulating a labeled image. Can be instantiated by
+ *    Support class for encapsulating a labeled image. Can only be instantiated from ImageParser API.
  */
 
 // Scala object - primarily used for built in factory method idioms
 object LabeledImage {
   // factory method
-  def apply(raw_image: String) = new LabeledImage
+  def apply(raw_image: String) = new LabeledImage(0, List(0), List(0), List(0))
 }
 
-class LabeledImage {
+class LabeledImage(label: Integer, redValues: List[Integer], greenValues: List[Integer], blueValues: List[Integer]) {
+
+  // L1 Distance function
+  def l1Distance(image: LabeledImage): Double = {
+
+  }
+
   // Label accessor
-  def label = "label"
+  def label: Integer = label // return instantiated label
 
   // Data accessor
   def data = "data"
--- src/main/scala/cs231n/nearestneighbor/NearestNeighbor.scala
+++ src/main/scala/cs231n/nearestneighbor/NearestNeighbor.scala
@@ -1,5 +1,7 @@
 package cs231n.nearestneighbor
 
+import cs231n.data.LabeledImage
+
 /*
  *    Nearest Neight Classifier
  *    -------------------------
@@ -8,11 +10,21 @@ package cs231n.nearestneighbor
  *    to naively predict the class of a given image (from predict).
  */
 class NearestNeighbor {
+  // Local variables
+  private var trainingImages: List[LabeledImage] = null
+
+  // L1 Distance measure -- defers calculation to Labeled Image
+  def l1Distance(x1: LabeledImage, x2: LabeledImage): Double = {
+    return x1.l1Distance(x2)
+  }
+
   // Train
-  def train(train_images: Unit, train_labels: Unit) = sys.error("NearestNeighbor.train not implmented")
+  def train(training_images: List[LabeledImage]) = {
+    trainImages = trainin_images // remember all input data
+  }
 
   // Predict
-  def predict(model: Unit, test_images: Unit) = sys.error("NearestNeighbor.predict not implmented")
+  def predict(model: Unit, test_images: List[LabeledImage]) = sys.error("NearestNeighbor.predict not implmented")
 
-  def hello = "Yep, still compiles and runs!"
+  def hello = "Yep, still compiles and runs! - "
 }
