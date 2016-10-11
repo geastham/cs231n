636bee93635ca0d4ca00cb92d92ec9a8f5165a7a
Garrett Eastham <garrett.eastham@gmail.com>
2016-10-10 21:40:12 -0500

    initial neural network model

--- src/main/scala/cs231n/nn/NeuralNetwork.scala
+++ src/main/scala/cs231n/nn/NeuralNetwork.scala
@@ -28,11 +28,11 @@ import breeze.numerics._
 class NeuralNetwork(
   // Layer 1
   var W_1: DenseMatrix[Double],
-  var b_1: Double,
+  var b_1: DenseVector[Double],
 
   //Layer 2
   var W_2: DenseMatrix[Double], 
-  var b_2: Double) {
+  var b_2: DenseVector[Double]) {
 
 }
 
@@ -68,7 +68,7 @@ object NeuralNetwork {
    *  @return success -- Boolean flag determining whether traiing was successful
    */
   
-  def train(training_images: Array[LabeledImage], number_of_classes: Integer): Boolean = {
+  def train(training_images: Array[LabeledImage]): Boolean = {
     // Perform Bias Trick on training data -- labels: Int, data: DenseVector[Double] - ((D + 1) x 1))
     val biased_training_data = training_images.map(i => {
       DenseVector(i.data)
@@ -76,15 +76,6 @@ object NeuralNetwork {
 
     // Generate training labels
     val training_labels = training_images.map(i => i.label)
-
-    // Initialize layer matrices - W_1
-    println("Initializing network layers...")
-    var W_1 = DenseMatrix.rand(number_of_classes * 2, biased_training_data(0).length) * (1.0 / Math.sqrt(number_of_classes * 2.0 * biased_training_data(0).length)) 
-    println(W_1)
-
-    // Initialize layer matrices - W_2
-    var W_2 = DenseMatrix.rand(number_of_classes, number_of_classes * 2) * (1.0 / Math.sqrt(number_of_classes * number_of_classes * 2.0))
-    println(W_2)
 
     // Return status of training
     return true
@@ -98,10 +89,38 @@ object NeuralNetwork {
   /*
    *  Apply
    *  -----------------
-   *  @param training_images -- set of training images to train
-   *  @param number_of_classes -- total number of distinct classes in training / test set (for simplicity, we set the output layer to this size)
+   *  @param input_size -- size of input dataset (for images this will be the flattened training image)
+   *  @param hidden_size -- size of intermediate (hidden) layer
+   *  @param output_size -- total number of distinct classes in training / test set (for simplicity, we set the output layer to this size)
    */
-   def apply(input_size: Integer, hidden_size: Integer, number_of_classes: Integer): Unit {
-    
-   }
+  def apply(input_size: Integer, hidden_size: Integer, output_size: Integer, std: Double = 0.0001): NeuralNetwork = {
+    println("Initializing network layers...")
+
+    // 1) Initialize layer 1 matrices - W_1
+    var W_1 = DenseMatrix.rand(input_size, hidden_size) * Math.sqrt(2.0 / input_size) 
+    println("\n1) W_1...")
+    println(W_1)
+
+    // 2) Initialize layer 1 bias -- B_1
+    var b_1 = DenseVector.zeros[Double](input_size)
+    println("\n2) b_1...")
+    println(b_1)
+
+    // 3) Initialize layer 2 matrices - W_2
+    var W_2 = DenseMatrix.rand(hidden_size, output_size) * Math.sqrt(2.0 / input_size) 
+    println("\n3) W_2...")
+    println(W_2)
+
+    // 4) Initialize layer 2 matrixes -- B_1
+    var b_2 = DenseVector.zeros[Double](hidden_size)
+    println("\n4) b_1...")
+    println(b_2)
+
+    // Return new NeuralNetwork
+    return new NeuralNetwork(W_1, b_1, W_2, b_2)
+  }
 }
+
+
+
+
