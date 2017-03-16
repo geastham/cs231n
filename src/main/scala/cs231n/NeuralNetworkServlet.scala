7bd0cabb4247fd6fb2f951b758517f9f590fd7b5
Garrett Eastham <garrett.eastham@gmail.com>
2017-03-16 11:15:57 -0500

    pushing -- still dependency issues

--- src/main/scala/cs231n/NeuralNetworkServlet.scala
+++ src/main/scala/cs231n/NeuralNetworkServlet.scala
@@ -11,20 +11,26 @@ import org.apache.spark.SparkConf
 // Scala utils
 import scala.util.Try
 
+// BigDL Imports
+import com.intel.analytics.bigdl._
+import com.intel.analytics.bigdl.utils.Engine
+
 // Create SearchServlet
 class NeuralNetworkServlet extends ScalatraServlet {
 
   // Initialize Spark context.
   // http://spark.apache.org/docs/1.2.0/programming-guide.html#initializing-spark
-  val conf = new SparkConf().setAppName("CS231n").setMaster("local").set("spark.driver.memory", "2g").set("spark.executor.memory", "2g")
-  val sc = new SparkContext(conf)
-
-  // Set SQL Context
-  val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-
-  // Spark SQL imports
-  import sqlContext.implicits._
-  import org.apache.spark.sql.SchemaRDD
+  //val conf = new SparkConf().setAppName("CS231n").setMaster("local").set("spark.driver.memory", "2g").set("spark.executor.memory", "2g")
+  //val sc = new SparkContext(conf)
+  val sc = Engine.init(1, 4, true).map(conf => {
+    conf.setAppName("CS231n")
+      .setMaster("local")
+      .set("spark.driver.memory", "2g")
+      .set("spark.executor.memory", "2g")
+      .set("spark.akka.frameSize", 64.toString)
+      .set("spark.task.maxFailures", "1")
+    new SparkContext(conf)
+  })
 
   // This defines the HTTP GET method that invokes Spark.
   get("/hello/:name") {
